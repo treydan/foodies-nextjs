567e8eb1f47de8a46ab4dbcc4dccfbe2111e541b
Trey Jamison <trey.jamison@gmail.com>
2026-01-15 10:57:49 -0600

    create meals page components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,24 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const MealsPage = () => {
   return (
-    <div>
-      <h1>Meals</h1>
-      <p>Explore our delicious meals!</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Explore our delicious meals!</p>
+        <div className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
