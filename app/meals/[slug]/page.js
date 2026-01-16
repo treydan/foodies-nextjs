7a20cc71bed89e25123e4ac86db2fef45b7211bc
Trey Jamison <trey.jamison@gmail.com>
2026-01-15 21:54:55 -0600

    add markup to meals page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,33 @@
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+
 const MealDetailPage = ({ params: { slug } }) => {
+  const meal = getMeal(slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+
   return (
-    <div>
-      <h1>Meal Details</h1>
-      <p>Here you can find more information about the meal: {slug}</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.create_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 };
 
